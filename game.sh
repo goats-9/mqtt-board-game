95f4507d973be851a7231b8eebd5da2b7e744229
debikaSamanta <114069684+debikaSamanta@users.noreply.github.com>
2024-04-22 22:30:25 +0530

    Update game.sh

--- game.sh
+++ game.sh
@@ -6,8 +6,10 @@ filename="player-1.txt"
 # Read the number of players from player-1.txt
 num_players=$(head -n 1 "$filename")
 
-# Start the MQTT publisher for each player
+# Start the MQTT publisher for each player in the background
 for ((i = 1; i <= $num_players; i++)); do
-    gnome-terminal -- bash -c "python3 pub.py $i 'player-$i.txt'; exec bash" &
+    python3 pub.py "$i" "player-$i.txt" &
 done
+
+# Wait for all background processes to finish
 wait
